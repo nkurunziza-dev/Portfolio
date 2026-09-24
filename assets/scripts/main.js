7a33afff93d2d1e6f0d868da819d1c55c733ee43
Kevin G. Nkurunziza <gknkurunziza.dev@gmail.com>
2026-09-24 17:23:39 +0300

    Work section styling

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -64,6 +64,22 @@ window.addEventListener('scroll', scrollHeader)
 
 
 /*=============== SWIPER WORK ===============*/ 
+const swiperWork = new Swiper('.work__swiper', {
+   loop: true,
+   spaceBetween: 24,
+   slidesPerView: 'auto',
+   grabCursor: true,
+   speed: 600,
+
+   pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+   },
+   autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+   }
+});
 
 
 /*=============== SERVICES ACCORDION ===============*/ 
